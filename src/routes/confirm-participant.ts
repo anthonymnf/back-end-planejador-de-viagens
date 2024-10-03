3d545848cceaac014bff6f8214d0fe581c3e0790
anthonymnf <anthonymnf30@gmail.com>
2024-10-03 16:20:18 -0300

    :bug: fix: Correção de bug na rota confirm-participants

--- src/routes/confirm-participant.ts
+++ src/routes/confirm-participant.ts
@@ -1,13 +1,13 @@
-import type { FastifyInstance } from 'fastify'
-import type { ZodTypeProvider } from 'fastify-type-provider-zod'
-import { z } from 'zod'
-import { prisma } from '../lib/prisma'
-import { ClientError } from '../errors/client-error'
-import { env } from '../env'
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { ClientError } from "../errors/client-error";
+import { env } from "../env";
 
 export async function confirmParticipants(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/participants/:participantId/confirm',
+  app.withTypeProvider<ZodTypeProvider>().patch(
+    "/participants/:participantId/confirm",
     {
       schema: {
         params: z.object({
@@ -16,28 +16,30 @@ export async function confirmParticipants(app: FastifyInstance) {
       },
     },
     async (request, reply) => {
-      const { participantId } = request.params
+      const { participantId } = request.params;
 
       const participant = await prisma.participant.findUnique({
         where: {
           id: participantId,
-        }
-      })
+        },
+      });
 
       if (!participant) {
-        throw new ClientError('Participant not found.')
+        throw new ClientError("Participant not found.");
       }
 
       if (participant.is_confirmed) {
-        return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`)
+        return reply.redirect(
+          `${env.WEB_BASE_URL}/trips/${participant.trip_id}`
+        );
       }
 
       await prisma.participant.update({
         where: { id: participantId },
-        data: { is_confirmed: true }
-      })
+        data: { is_confirmed: true },
+      });
 
-      return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`)
-    },
-  )
+      return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`);
+    }
+  );
 }
