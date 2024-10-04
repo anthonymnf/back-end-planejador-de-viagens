bc006637797cccf1307ef1dc57edad7a2d43431c
anthonymnf <anthonymnf30@gmail.com>
2024-10-04 13:06:04 -0300

    :sparkles: feat: Configurando o envio do email de confirmação

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,17 +1,15 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
 export async function getMailClient() {
-  const account = await nodemailer.createTestAccount()
-
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
+    host: "smtp.gmail.com",
     port: 587,
     secure: false,
     auth: {
-      user: account.user,
-      pass: account.pass,
+      user: process.env.ENDERECO_GMAIL,
+      pass: process.env.SENHA_DE_APP,
     },
-  })
+  });
 
-  return transporter
+  return transporter;
 }
